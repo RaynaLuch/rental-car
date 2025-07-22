5cc5ecef423ff1ad88358dbda2f5b52a9ff6862f
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2025-07-22 09:47:24 +0300

    Create  HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Layout from "./components/Layout/Layout.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+//import CatalogPage from "./pages/CatalogPage/CatalogPage.jsx";
+//import CarPage from "./pages/CarPage/CarPage.jsx";
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Layout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/catalog/:id" element={<CarPage />} /> */}
+        </Routes>
+      </Suspense>
+    </Layout>
+  );
 }
-
-export default App
